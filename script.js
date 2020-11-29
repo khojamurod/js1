bf7a0ea84cab784ff9ca623690d7f8101b295158
khojakapro <azam188003@gmail.com>
2020-11-29 16:15:13 +0500

    java-v1.0

---
+++ script.js
@@ -0,0 +1,29 @@
+var ism =  prompt ('Ismingiz :');
+var surname = prompt ('Familiyangiz :');
+alert(surname +" "+ ism);
+var age = prompt('Yoshingiz :');
+
+    console.log('Sizni ismingiz :'+ism);
+    console.log('Sizni familiyangiz :'+surname );
+    console.log('Siz ' + age + ' yoshtasiz');
+
+var ex1t ='1+1+1:1+1*1+1^2';
+var ex2t ='12+3^2-1';
+var ex3t ='11+1-2^2';
+var ex4t ='120*3-120';
+var ex5t ='360-20+20*2^((3+2:8+24/3+22.11- 33.33333)*0)';
+
+var ex1 = prompt(ex1t+'= ?');
+var ex2 = prompt(ex2t+'= ?');
+var ex3 = prompt(ex3t+'= ?');
+var ex4 = prompt(ex4t+'= ?');
+var ex5 = prompt(ex5t+'= ?');
+
+alert( 'Javoblarni korish uchun "console"ga oting');
+
+    console.log('1 misol '+ex1t+'ning togri javobi- 5 ' +'sizning javobingiz esa '+ ex1 +' edi ' );
+    console.log('2 misol '+ex2t+'ning togri javobi- 20 '+'sizning javobingiz esa '+ ex2 +' edi ' );
+    console.log('3 misol '+ex3t+'ning togri javobi- 16 '+'sizning javobingiz esa '+ ex3 +' edi ' );
+    console.log('4 misol '+ex4t+'ning togri javobi- 240 '+'sizning javobingiz esa '+ex4 +' edi ' );
+    console.log('5 misol '+ex5t+'ning togri javobi- 360 '+'sizning javobingiz esa '+ex5 +' edi ' );
+    
